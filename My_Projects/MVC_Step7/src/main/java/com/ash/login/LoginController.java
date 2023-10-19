ccb2aefe927ac13ede95798aa2c245a94b39f714
Ash <aswathyspa@gmail.com>
2023-10-19 14:10:48 -0700

    Minimal req

--- My_Projects/MVC_Step7/src/main/java/com/ash/login/LoginController.java
+++ My_Projects/MVC_Step7/src/main/java/com/ash/login/LoginController.java
@@ -1,24 +1,35 @@
-package com.ash.springmvc;
+package com.ash.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ash.login.LoginService;
 
 @Controller
 public class LoginController {
+	@Autowired
+	LoginService service;
+	
 	@RequestMapping(value="/login",method=RequestMethod.GET)
 	public String showLoginPage()
 	{
 		return "login";
 	}
 	@RequestMapping(value="/login",method=RequestMethod.POST)
-	public String handleloginRequest(@RequestParam String name, ModelMap model)
+	public String handleloginRequest(@RequestParam String name,@RequestParam String password, ModelMap model)
 	{
+		if(service.validateUser(name, password))
+		{	
 		model.put("name", name);
+		model.put("password", password);
 		return "welcome";
+	}
+		model.put("errorMessage", "Invalid Credentials");
+		return "login";
 	}
 
 }
